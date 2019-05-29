a28c3822a57412c8e807cc8746e7a34e848c8123
xuepeng <xuepeng@yesway.cn>
2019-05-29 16:39:53 +0800

    [xuepeng]加入用户、角色与菜单的新功能。

--- pms-backend/src/main/java/cc/xuepeng/entity/RoleMenuRelationCondition.java
+++ pms-backend/src/main/java/cc/xuepeng/entity/RoleMenuRelationCondition.java
@@ -2,7 +2,6 @@ package cc.xuepeng.entity;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class RoleMenuRelationCondition implements Serializable {
